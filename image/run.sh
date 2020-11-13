e47d4b2315c61e16b6773bac90153a945c4e216a
John Westcott IV <john.westcott.iv@redhat.com>
2020-11-13 14:12:05 -0500

    Initial build out, still need to do the terraform init pieces

--- image/run.sh
+++ image/run.sh
@@ -5,11 +5,13 @@ export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 # Check to see if this is the first time we have run, and set up the DB if necessary
 if [ ! -f /var/lib/pgsql/data/db_initalized ]
 then
-  sudo postgresql-setup --initdb
-  
+  sudo /usr/bin/postgresql-setup --initdb
+  cat "local   all             all                                     trust" > /var/lib/pgsql/data/pg_hba.conf
   touch /var/lib/pgsql/data/db_initalized
 fi
 
+# start postgres deamon
+/usr/bin/postmaster -D /var/lib/pgsql/data
 
-
-
+# Run sshd in the foreground
+sudo /usr/sbin/sshd -D
