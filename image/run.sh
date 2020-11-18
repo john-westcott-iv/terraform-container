e0718c69defb7b35e846cc4c9e820592d6b8e1b1
John Westcott IV <john.westcott.iv@redhat.com>
2020-11-18 13:21:03 -0500

    Adding chmod and moving chown commands outside of the if

--- image/run.sh
+++ image/run.sh
@@ -4,11 +4,14 @@ export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 export DB_INIT_FILE=/var/lib/pgsql/data/db_initalized
 export TF_DB_INIT_FILE=/var/lib/pgsql/data/tf_created
 
+# make sure our permissions are set on the PVCs
+sudo /usr/bin/chown postgres.0 /var/lib/pgsql
+/usr/bin/chmod 700 /var/lib/pgsql
+sudo /usr/bin/chown terraform.0 /home/terraform/terraform_scratch
+
 # Check to see if this is the first time we have run, and set up the DB if necessary
 if [ ! -f ${DB_INIT_FILE} ]
 then
-  sudo /usr/bin/chown postgres.0 /var/lib/pgsql
-  sudo /usr/bin/chown terraform.0 /home/terraform/terraform_scratch
   echo "Initializing DB"
   initdb -D /var/lib/pgsql/data && touch ${DB_INIT_FILE}
 fi
