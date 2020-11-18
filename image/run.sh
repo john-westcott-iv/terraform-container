ace93ee94c201649c6b1d46646c5d24d01dc5de3
John Westcott IV <john.westcott.iv@redhat.com>
2020-11-18 08:49:30 -0500

    Multiple changes
    Adding echos so we know what the container is doing
    
    Changing from from postgresql-setup to initdb due to sytemd dependencies on postgresql-setup
    
    Fixing bugs with putting postgre in background and writing out the pg_hba.conf
    
    Variablized the DB init file

--- image/run.sh
+++ image/run.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
+export DB_INIT_FILE=/var/lib/pgsql/data/db_initalized
 
 # Check to see if this is the first time we have run, and set up the DB if necessary
-if [ ! -f /var/lib/pgsql/data/db_initalized ]
+if [ ! -f ${DB_INIT_FILE} ]
 then
-  sudo /usr/bin/postgresql-setup --initdb
-  cat "local   all             all                                     trust" > /var/lib/pgsql/data/pg_hba.conf
-  touch /var/lib/pgsql/data/db_initalized
+  echo "Initializing DB"
+  initdb -D /var/lib/pgsql/data
+  echo "Create pg_hba.conf"
+  echo "local   all             all                                     trust" > /var/lib/pgsql/data/pg_hba.conf
+  touch ${DB_INIT_FILE}
 fi
 
 # start postgres deamon
-/usr/bin/postmaster -D /var/lib/pgsql/data
+echo "Starting pgsql"
+/usr/bin/postmaster -D /var/lib/pgsql/data &
 
 # Run sshd in the foreground
+echo "Starting SSHD"
 sudo /usr/sbin/sshd -D
